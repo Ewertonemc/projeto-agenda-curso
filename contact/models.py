ac3b496b26415c10c7260be86be40ee1b149f0d7
Ewerton Cunha <ewertonemc@gmail.com>
2025-06-17 20:56:58 -0300

    alterando configuracao de admin.py e correcao no models.py

--- contact/models.py
+++ contact/models.py
@@ -13,7 +13,7 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
-    crated_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
 
     def __str__(self) -> str:
