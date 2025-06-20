ba3b33e74b5e804f1af4dd1d4f6f046546768348
Ewerton Cunha <ewertonemc@gmail.com>
2025-06-20 13:03:58 -0300

    criado forms, create e update

--- contact/urls.py
+++ contact/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
 
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
 
-    path('<int:contact/contact_id>/', views.contact, name='contact'),
-    path('<contact/create/', views.contact, name='contact'),
-    path('<int:contact/contact_id>/update/', views.contact, name='contact'),
-    path('<int:contact/contact_id>/delete/', views.contact, name='contact'),
 ]
