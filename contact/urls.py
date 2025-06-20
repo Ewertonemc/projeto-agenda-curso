ea97f533061785b8fc1b171202039bb4173181aa
Ewerton Cunha <ewertonemc@gmail.com>
2025-06-19 22:32:06 -0300

    partials, pagination,...

--- contact/urls.py
+++ contact/urls.py
@@ -4,7 +4,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
 
+
+    path('<int:contact/contact_id>/', views.contact, name='contact'),
+    path('<contact/create/', views.contact, name='contact'),
+    path('<int:contact/contact_id>/update/', views.contact, name='contact'),
+    path('<int:contact/contact_id>/delete/', views.contact, name='contact'),
 ]
